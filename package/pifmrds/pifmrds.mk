2a6a669e370a72e1834ec5880307ca79ef9820ab
pho <phofin@gmail.com>
2014-08-02 18:24:37 +0200

    PiFmRds Works

--- package/pifmrds/pifmrds.mk
+++ package/pifmrds/pifmrds.mk
@@ -6,11 +6,11 @@ PIFMRDS_VERSION = a985b7db02338b1a3073a15f292cec0e2ae3343a
 PIFMRDS_SITE = $(call github,ChristopheJacquet,PiFmRds,$(PIFMRDS_VERSION))
 
 define PIFMRDS_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC) LD="$(TARGET_LD)" -C $(@D)/src
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C "$(@D)/src" app
 endef
 
 define PIFMRDS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/src/pi_fm_rds $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 "$(@D)/src/pi_fm_rds" "$(TARGET_DIR)/usr/bin/"
 endef
 
 $(eval $(generic-package))
