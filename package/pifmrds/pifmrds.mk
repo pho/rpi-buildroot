7d5010f4cb5b94deb0cfc3da0c514221ee81cf96
pho <phofin@gmail.com>
2014-08-02 16:54:07 +0200

    Added PiFmRds package

---
+++ package/pifmrds/pifmrds.mk
@@ -0,0 +1,16 @@
+######################
+# pifmrds
+##################
+
+PIFMRDS_VERSION = a985b7db02338b1a3073a15f292cec0e2ae3343a
+PIFMRDS_SITE = $(call github,ChristopheJacquet,PiFmRds,$(PIFMRDS_VERSION))
+
+define PIFMRDS_BUILD_CMDS
+	$(MAKE) CC="$(TARGET_CC) LD="$(TARGET_LD)" -C $(@D)/src
+endef
+
+define PIFMRDS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/src/pi_fm_rds $(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(generic-package))
